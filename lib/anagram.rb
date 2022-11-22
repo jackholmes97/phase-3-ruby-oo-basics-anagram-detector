c0b9d9c04ea9034de0066da27a7ae1abd6b9b508
Jack Holmes <jdh97@comcast.net>
2022-11-22 11:31:23 -0500

    commit tree 84d21ee88d2bc89d6e9679431fb22a5b13aa4c24

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram 
+    attr_reader :word
+    # attr_accessor :list
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        anagrams = []
+        wordArr = @word.chars
+        list.map do |option|
+            op_arr = option.chars
+            if (op_arr.sort == wordArr.sort)
+                return anagrams.push(option)
+            end
+        end
+        anagrams
+    end
+
+
+end
+
+
